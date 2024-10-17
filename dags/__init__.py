6c7b9a1e8a5e5fd2af3ce5e966e88e2b5f14cd82
jungmarker <146157814+jungmarker@users.noreply.github.com>
2024-10-17 00:47:30 +0000

    dags module

---
+++ dags/__init__.py
@@ -0,0 +1,5 @@
+# This file makes the dags directory a Python package/module.
+# You can include imports or setup code here if necessary.
+
+# Importing the necessary scripts to be available when importing dags
+from .scripts import etl_workflow
